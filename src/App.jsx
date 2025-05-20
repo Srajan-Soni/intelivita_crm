93ddf138829ae9e9d9d7103855cdce4908e7c8cf
Srajan-Soni <60183046+Srajan-Soni@users.noreply.github.com>
2025-05-20 16:38:01 +0530

    app initiated

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center mb-4">
+        CRM JSON Data
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
